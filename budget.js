08790788374bbfa5bf51c1f49bd8c653a03a8e5c
曾國峻 <guojiun@cengguojuns-MacBook-Air.local>
2018-10-18 14:53:33 +0800

    Use loop to cover all scenarios

--- budget.js
+++ budget.js
@@ -40,30 +40,16 @@ export class BudgetPlan {
     }
 
     _query(period) {
-
-        if (period.start.isSame(period.end, 'month')) {
-            let firstBudget = new Budget(period.start.format('YYYY-MM'), this.budgets[period.start.format('YYYY-MM')]);
-            return firstBudget._getAmountOfOverlapping(period)
-        }
         let totalAmount = 0
-
-        // start month
-        let firstBudget = new Budget(period.start.format('YYYY-MM'), this.budgets[period.start.format('YYYY-MM')]);
-        totalAmount += firstBudget._getAmountOfOverlapping(period)
-
-        // months in between
-        const monthDiff = period.end.diff(period.start, 'months') - 1
-        for (let month = 1; month <= monthDiff; month++) {
+        
+        const monthDiff = period.end.diff(period.start, 'months') + 1
+        for (let month = 0; month <= monthDiff; month++) {
             const monthString = moment(period.start)
                 .add(month, 'month')
                 .format('YYYY-MM')
             let budget = new Budget(monthString, this.budgets[monthString]);
             totalAmount += budget._getAmountOfOverlapping(period)
         }
-
-        // end month
-        let lastBudget = new Budget(period.end.format('YYYY-MM'), this.budgets[period.end.format('YYYY-MM')]);
-        totalAmount += lastBudget._getAmountOfOverlapping(period)
         return totalAmount
     }
 }
