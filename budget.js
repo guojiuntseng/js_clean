58f18f8145269e7f6d34bd100f7aae6c04b45b40
曾國峻 <guojiun@cengguojuns-MacBook-Air.local>
2018-10-18 11:05:00 +0800

    Extract class Period to avoid Data Clump

--- budget.js
+++ budget.js
@@ -1,32 +1,41 @@
 import moment from 'moment'
 
+class Period {
+    constructor (start, end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+
 export class Budget {
     budgets = {}
 
     query(startDate, endDate) {
+        this._query(new Period(moment(startDate, 'YYYY-MM-DD'), moment(endDate, 'YYYY-MM-DD')));
+    }
 
-        const momentStartDate = moment(startDate, 'YYYY-MM-DD')
-        const momentEndDate = moment(endDate, 'YYYY-MM-DD')
+    _query(period) {
 
-        if (momentStartDate.isSame(momentEndDate, 'month')) {
-            return this._getAmountOfPeriod(momentStartDate, momentEndDate)
+        if (period.start.isSame(period.end, 'month')) {
+            return this._getAmountOfPeriod(period.start, period.end)
         } else {
             let budget = 0
 
             // start month
-            budget += this._getAmountOfPeriod(momentStartDate, moment(momentStartDate).endOf('month'))
+            budget += this._getAmountOfPeriod(period.start, moment(period.start).endOf('month'))
 
             // months in between
-            const monthDiff = momentEndDate.diff(momentStartDate, 'months') - 1
+            const monthDiff = period.end.diff(period.start, 'months') - 1
             for (let month = 1; month <= monthDiff; month++) {
-                const monthString = moment(startDate, 'YYYY-MM-DD')
+                const monthString = moment(period.start)
                     .add(month, 'month')
                     .format('YYYY-MM')
                 budget += this.budgets[monthString] || 0
             }
 
             // end month
-            budget += this._getAmountOfPeriod(moment(momentEndDate).startOf('month'), momentEndDate)
+            budget += this._getAmountOfPeriod(moment(period.end).startOf('month'), period.end)
             return budget
         }
     }
