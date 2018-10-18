34119c7245a2f5f54657ad706097a4660c9fe3ff
曾國峻 <guojiun@cengguojuns-MacBook-Air.local>
2018-10-18 11:26:14 +0800

    Move day count to Period to avoid Feature Envy

--- budget.js
+++ budget.js
@@ -5,6 +5,10 @@ class Period {
         this.start = start;
         this.end = end;
     }
+
+    dayCount() {
+        return this.end.diff(this.start, 'days') + 1
+    }
 }
 
 
@@ -41,9 +45,9 @@ export class Budget {
     }
 
     _getAmountOfPeriod(period) {
-        const diffDays = period.end.diff(period.start, 'days') + 1
+        const diffDays = period.dayCount;
         const dayCountOfBudget = period.start.daysInMonth();
-        const amountOfBudget = (this.budgets[period.start.format('YYYY-MM')] || 0);
+        const amountOfBudget = this.budgets[period.start.format('YYYY-MM')] || 0;
         return amountOfBudget / dayCountOfBudget * diffDays;
     }
 }
